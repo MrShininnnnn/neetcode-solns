1856cf6e67a2a0e159bf97756d91d20fcee0a380
Shining <mrshininnnnn@gmail.com>
2026-07-13 23:18:29 -0600

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2:
+            return False
+        
+        stack = []
+        chars_dict = { "(" : ")", "[" : "]", "{" : "}" }
+
+        for c in s:
+            if c in chars_dict:
+                stack.append(c)
+            else:
+                if stack and c == chars_dict[stack[-1]]:
+                    stack.pop()
+                else:
+                    return False
+                
+        return not stack
